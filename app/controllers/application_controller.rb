5b6d0c51f0422058de4d5bc971e8a32bab9264c1
Neal Howarth <neal@ripepixel.co.uk>
2012-10-01 16:42:17 +0100

    Meta stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :meta_defaults
   
   def authorise
     redirect_to login_url, alert: "Not authorised" if current_user.nil?
@@ -10,4 +11,10 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
     helper_method :current_user
+
+    def meta_defaults
+    	@meta_keys = "crafts, arts crafts, kids crafts, childrens crafts, crafts for kids, crafts for children, craft activities"
+    	@meta_desc = "Kids craft items delivered monthly. All arts and craft materials are included to keep the children entertained for hours. Receive a packed craft box every month, with free delivery."
+    end
+    
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,6 @@ module ApplicationHelper
   def page_title
   	(@page_title + " - " if @page_title).to_s + 'Little Busy Bees - Childrens Craft Boxes'
   end
-
-  def meta_keys
-  	(@meta_keys = "" if !@meta_keys).to_s
-  end
-
-  def meta_desc
-  	(@meta_desc = "" if !@meta_desc).to_s
-  end
   
   def latest_blog_posts
   	Article.where("published = ?", true).order("created_at DESC").limit(4)
