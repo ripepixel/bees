1f8414f30626398811575dcd31c47ec9a7ed6362
Neal Howarth <neal@ripepixel.co.uk>
2012-10-18 20:43:08 +0100

    more problems

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,8 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "https://github.com/ripepixel/bees.git"
+set :repository, "git@github.com:ripepixel/bees.git"
+# set :repository, "https://github.com/ripepixel/bees.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
