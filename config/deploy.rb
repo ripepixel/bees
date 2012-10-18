c087bb8a493cecf142fc353dc1eccc1cb6997693
Neal Howarth <neal@ripepixel.co.uk>
2012-10-18 20:16:38 +0100

    Deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,6 +47,9 @@ end
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
+  after :deploy do
+    run "chmod -R g+w #{release_path}/tmp"
+  end
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
